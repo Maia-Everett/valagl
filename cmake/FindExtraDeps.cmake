85e29e688f4f26d3634cc657934ed389e0c4ab80
Maia Everett <maia@everett.one>
2013-03-30 19:02:21 +0700

    Add Valadoc generation

--- cmake/FindExtraDeps.cmake
+++ cmake/FindExtraDeps.cmake
@@ -1,10 +1,11 @@
 message("-- Checking for libgee...")
 pkg_check_modules(GEE gee-1.0)
 
-if(GEE_FOUND)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(ExtraDeps DEFAULT_MSG GEE_INCLUDE_DIRS)
+
+if(EXTRADEPS_FOUND)
     set(EXTRA_DEP_INCLUDES ${EXTRA_DEP_INCLUDES} ${GEE_INCLUDE_DIRS})
     set(EXTRA_DEP_CFLAGS ${EXTRA_DEP_CFLAGS} ${GEE_CFLAGS_OTHER})
     set(EXTRA_DEP_LIBS ${EXTRA_DEP_LIBS} ${GEE_LDFLAGS})
-else()
-    message(FATAL_ERROR "libgee not found")
 endif()
